3baa0f04124cd8fd8900f53df16575b3a0aec421
Konrad <konradsawicki98@gmail.com>
2023-02-23 16:10:31 +0100

    added forgotten const to functions

--- CarDealer/TimeManager.h
+++ CarDealer/TimeManager.h
@@ -8,13 +8,13 @@ class TimeManager
 {
 public:
 	template<typename T>
-	T CalculateDuration(high_resolution_clock::time_point tp1, high_resolution_clock::time_point tp2)
+	T CalculateDuration(high_resolution_clock::time_point tp1, high_resolution_clock::time_point tp2) const
 	{
 		return duration_cast<T>(tp1 - tp2);
 	}
 
 
-	high_resolution_clock::time_point Now()
+	high_resolution_clock::time_point Now() const
 	{
 		return high_resolution_clock::now();
 	}
